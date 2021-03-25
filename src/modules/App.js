dbb191d477bd9d0c431e3049cba671d793662fb5
lexx54 <chapelexx54@gmail.com>
2021-03-25 12:02:52 -0400

    created new function to fecth pokemon data

--- src/modules/App.js
+++ src/modules/App.js
@@ -4,40 +4,57 @@ import "./app.css";
 
 const DEFFAULT_URL = "https://pokeapi.co/api/v2/";
 const DEFAULT_TYPE ="pokemon/";
-
-
+const POKEMON_LIST=[
+  "charizard",
+  'ditto',
+  'charmeleon',
+  'alakazam'
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      data: {}
+      pokemonName:"charizard",
+      pokemonData: {}
     };
-
+    this.getPokemon=this.getPokemon.bind(this);
+    this.fetchPokemonData=this.fetchPokemonData.bind(this);
   }
 
-  componentDidMount() {
+  getPokemon(){
+    console.log("getting pokemon");
+    const randomNumber=Math.floor(Math.random()*POKEMON_LIST.length);
+    this.setState({pokemonName:POKEMON_LIST[randomNumber]})
+    this.fetchPokemonData();
+  }
+
+  fetchPokemonData(){
     setTimeout(() => {
-      fetch(DEFFAULT_URL + DEFAULT_TYPE + "charizard")
+      fetch(DEFFAULT_URL + DEFAULT_TYPE + this.state.pokemonName)
         .then(res => res.json())
         .then(res => this.setState({
           isLoading: false,
-          data: res
+          pokemonData: res
         }))
     }, 2000)
   }
 
+  componentDidMount() {
+    this.fetchPokemonData();
+  }
+
   render() {
-    const { isLoading, data } = this.state;
-    console.log(data);
+    const { isLoading, pokemonData } = this.state;
+    console.log(pokemonData);
     if (isLoading) {
       return <h1>Loading...</h1>
     }
     return (
       <div className="principal-page">
-        <Pokemon data={data} />
-        <Pokemon data={data} />
+        <Pokemon pokemonData={pokemonData} action={this.getPokemon}/>
+        <Pokemon pokemonData={pokemonData} action={this.getPokemon}/>
       </div>
     )
   }
--- src/modules/pokemon/index.js
+++ src/modules/pokemon/index.js
@@ -1,15 +1,18 @@
+import React, {useState} from "react";
 import Stats from "./stats";
 import Info from "./info";
 import Button from "./buttons"
 import "./pokemon.css";
 
-const Pokemon = ({ data }) => (
+const Pokemon = ({ pokemonData ,action}) => {
+  (
   <section className="pokemon">
-    <h2>{data.name.toUpperCase()}</h2>
-    <Info sprites={data.sprites} id={data.id} experience={data.base_experience}/>
-    <Stats stats={data.stats} />
-    <Button type="" text="Generate"/>
+    <h2>{pokemonData.name.toUpperCase()}</h2>
+    <Info sprites={pokemonData.sprites} id={pokemonData.id} experience={pokemonData.base_experience}/>
+    <Stats stats={pokemonData.stats} />
+    <Button type="" text="Generate" action={action}/>
   </section>
-)
+  )
+}
 
 export default Pokemon;
